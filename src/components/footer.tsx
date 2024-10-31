da29e0f500d9e768b2317dfa254a8bba09421667
Khalil <khalilpati55@gmail.com>
2024-10-31 21:51:04 +0530

    Add basic footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-gray-100 py-6 dark:bg-gray-900">
+      <p className="text-center">
+        © {new Date().getFullYear()} - Click-n-Meet
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
