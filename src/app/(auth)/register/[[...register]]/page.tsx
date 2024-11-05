d644255957881a0466fb0c92d30595963a35dde4
Khalil <khalilpati55@gmail.com>
2024-11-05 23:57:43 +0530

    Add force redirect url after signup

--- src/app/(auth)/register/[[...register]]/page.tsx
+++ src/app/(auth)/register/[[...register]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 const RegisterPage = () => {
-  return <SignUp />;
+  return <SignUp forceRedirectUrl={"/dashboard"} />;
 };
 
 export default RegisterPage;
